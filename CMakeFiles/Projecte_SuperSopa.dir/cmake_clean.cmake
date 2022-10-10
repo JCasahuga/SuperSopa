fc6d4499bd103406592115b1f09d50feb7becf38
miquel.torner.vinals <miquel.torner.vinals@estudiantat.upc.edu>
2022-10-10 18:47:32 +0200

    no funciona :(

---
+++ CMakeFiles/Projecte_SuperSopa.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projecte_SuperSopa.dir/NodeTrie.cc.o"
+  "CMakeFiles/Projecte_SuperSopa.dir/NodeTrie.cc.o.d"
+  "CMakeFiles/Projecte_SuperSopa.dir/Trie.cc.o"
+  "CMakeFiles/Projecte_SuperSopa.dir/Trie.cc.o.d"
+  "CMakeFiles/Projecte_SuperSopa.dir/diccBloomFilter.cc.o"
+  "CMakeFiles/Projecte_SuperSopa.dir/diccBloomFilter.cc.o.d"
+  "CMakeFiles/Projecte_SuperSopa.dir/diccDHashing.cc.o"
+  "CMakeFiles/Projecte_SuperSopa.dir/diccDHashing.cc.o.d"
+  "CMakeFiles/Projecte_SuperSopa.dir/diccTrie.cc.o"
+  "CMakeFiles/Projecte_SuperSopa.dir/diccTrie.cc.o.d"
+  "CMakeFiles/Projecte_SuperSopa.dir/program.cc.o"
+  "CMakeFiles/Projecte_SuperSopa.dir/program.cc.o.d"
+  "Projecte_SuperSopa"
+  "Projecte_SuperSopa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projecte_SuperSopa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/Projecte_SuperSopa.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Projecte_SuperSopa.
+# This may be replaced when dependencies are built.
